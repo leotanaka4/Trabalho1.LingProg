e0170b6a914bfa47a771e495de0bf9e934b1ad60
Leonardo Tanaka <89543577+leotanaka4@users.noreply.github.com>
2022-01-06 12:55:22 -0300

    Update AppM.py

---
+++ AppM.py
@@ -0,0 +1,34 @@
+from tkinter import *
+from AppO import Obtidos
+from AppD import Desejados
+
+class Principal(Obtidos, Desejados):
+    def __init__(self, master=None):
+
+        self.fonte = ("Verdana", "8")
+        self.container1 = Frame(master)
+        self.container1["pady"] = 10
+        self.container1.pack()
+        self.container2 = Frame(master)
+        self.container2["padx"] = 20
+        self.container2["pady"] = 5
+        self.container2.pack()
+        self.container3 = Frame(master)
+        self.container3["padx"] = 20
+        self.container3["pady"] = 5
+        self.container3.pack()
+        
+        self.titulo = Label(self.container1, text="Bookaholic")
+        self.titulo["font"] = ("Calibri", "9", "bold")
+        self.titulo.pack ()
+
+        self.btnBuscar = Button(self.container2, text="Lista de Livros Obtidos",
+        font=self.fonte, width=10)
+        self.btnBuscar["command"] = self.iniciarObtidos
+        self.btnBuscar.pack(side=RIGHT)
+
+        self.btnBuscar = Button(self.container3, text="Lista de Livros Desejados",
+        font=self.fonte, width=10)
+        self.btnBuscar["command"] = self.iniciarDesejados
+        self.btnBuscar.pack(side=RIGHT)
+
