59765c23262683922583f938ef4113e1742058c9
Leonardo Tanaka <89543577+leotanaka4@users.noreply.github.com>
2022-01-06 13:13:13 -0300

    Update AppO.py

--- AppO.py
+++ AppO.py
@@ -63,11 +63,11 @@ class Obtidos:
         self.txtidlivro["font"] = self.fonte
         self.txtidlivro.pack(side=LEFT)
 
+        #Buscar
         self.btnBuscar = Button(self.container3, text="Buscar",
         font=self.fonte, width=10)
         self.btnBuscar["command"] = self.buscarlivro
         self.btnBuscar.pack(side=RIGHT)
-
 
         #Nome
         self.lblnome = Label(self.container4, text="Nome:",
@@ -78,24 +78,6 @@ class Obtidos:
         self.txtnome["width"] = 25
         self.txtnome["font"] = self.fonte
         self.txtnome.pack(side=LEFT)
-
-        '''self.lbltelefone = Label(self.container4, text="Telefone:",
-        font=self.fonte, width=10)
-        self.lbltelefone.pack(side=LEFT)
-
-        self.txttelefone = Entry(self.container4)
-        self.txttelefone["width"] = 25
-        self.txttelefone["font"] = self.fonte
-        self.txttelefone.pack(side=LEFT) '''
-
-        '''self.lblemail= Label(self.container5, text="E-mail:",
-        font=self.fonte, width=10)
-        self.lblemail.pack(side=LEFT)
-
-        self.txtemail = Entry(self.container5)
-        self.txtemail["width"] = 25
-        self.txtemail["font"] = self.fonte
-        self.txtemail.pack(side=LEFT)'''
 
         #Autor
         self.lblautor= Label(self.container5, text="Autor:",
@@ -137,17 +119,6 @@ class Obtidos:
         self.txtdataa["font"] = self.fonte
         self.txtdataa.pack(side=LEFT)
 
-
-        '''self.lblsenha= Label(self.container7, text="Senha:",
-        font=self.fonte, width=10)
-        self.lblsenha.pack(side=LEFT)
-
-        self.txtsenha = Entry(self.container7)
-        self.txtsenha["width"] = 25
-        self.txtsenha["show"] = "*"
-        self.txtsenha["font"] = self.fonte
-        self.txtsenha.pack(side=LEFT)'''
-
         tipo = StringVar()
         tipo.set(self.status[2])
         self.dropdown = OptionMenu(self.container10,tipo,*self.status,command=self.display_selected)
@@ -163,10 +134,6 @@ class Obtidos:
 
         self.bntExcluir = Button(self.container8, text="Excluir",font=self.fonte, width=12)
         self.bntExcluir["command"] = self.excluir
-        self.bntExcluir.pack(side=LEFT)
-
-        self.bntExcluir = Button(self.container8, text="Criar tela",font=self.fonte, width=12)
-        self.bntExcluir["command"] = self.criaTela
         self.bntExcluir.pack(side=LEFT)
 
         self.lblmsg = Label(self.container9, text="")
@@ -234,16 +201,6 @@ class Obtidos:
 
         self.txtdataa.delete(0, END)
         self.txtdataa.insert(INSERT,user.dataa)
-
-    def criaTela(self):
-        root2 = Tk()
-        root2.title("Nova Janela")
-        cont1 = Frame(root2)
-        cont1.pack()
-        titulo = Label(root2, text="Nova Tela")
-        titulo["font"] = ("Calibri", "9", "bold")
-        titulo.pack ()
-        root2.mainloop()
 
 root = Tk()
 Obtidos(root)
